e234b4f758677b14a5fb0f0dd29918b8f7ebc8f2
GarrettGutierrez <ruzhyo2000@gmail.com>
2016-01-21 15:40:19 -0800

    AI interface / attack collisions
    Worked on AI interface and attack registering.

--- CS194-Unity/Assets/Scripts/GameScript.cs
+++ CS194-Unity/Assets/Scripts/GameScript.cs
@@ -9,9 +9,11 @@ public class GameScript : MonoBehaviour {
 	public GameObject player1BodyBox;
 	public GameObject player1HitBox;
 	public GameObject player1BlockBox;
+	public bool player1AI;
 	public GameObject player2BodyBox;
 	public GameObject player2HitBox;
 	public GameObject player2BlockBox;
+	public bool player2AI;
 	public float forwardVelocity;
 	public float backwardVelocityFactor;
 	public float runningVelocityFactor;
@@ -19,12 +21,44 @@ public class GameScript : MonoBehaviour {
 	public float perspXPadding;
 	public float perspAngle;
 	public float perspWidthMinimum;
+	private Player player1;
+	private Player player2;
+
+	private class GameState
+	{
+	}
+	private class Action
+	{
+		public bool moving;
+		public bool movingAway;
+		public bool running;
+		public bool blocking;
+		public bool lowBlocking;
+		public int initiateAttack;
+		public Action() {
+			moving = false;
+			movingAway = false;
+			running = false;
+			blocking = false;
+			lowBlocking = false;
+			initiateAttack = 0;
+		}
+	}
+	private class AI
+	{
+		public Action queryAction(GameState state) {
+			Action resultAction = new Action();
+			return resultAction;
+		}
+	}
 	private class Player
 	{
+		// STAT VARIABLES
+		private float health;
+		// HITBOX VARIABLES
 		private GameObject playerBodyBox;
 		private GameObject playerHitBox;
 		private GameObject playerBlockBox;
-		private bool inputHold;
 		// MOVEMENT VARIABLES
 		private bool running;
 		private bool movingLeft;
@@ -37,17 +71,98 @@ public class GameScript : MonoBehaviour {
 		private float timeAttackBegins;
 		private float timeAttackEnds;
 		private float reach;
+		private float attackDamage;
 		private bool lowAttack;
+		// INFO VARIABLES
+		private bool inputHold;
+		private bool attackWasThrown;
+		private bool attackWasFinished;
+		private bool attackHit;
 		public Player() {
+			health = 1000.0f;
 			running = false;
 			movingLeft = false;
 			movingRight = false;
+			movingAway = false;
+			distanceMoved = 0.0f;
+			oldXPosition = 0.0f;
+			timeEnds = 0.0f;
+			timeAttackBegins = 0.0f;
+			timeAttackEnds = 0.0f;
+			reach = 0.0f;
+			attackDamage = 0.0f;
+			lowAttack = false;
 			inputHold = false;
+			attackWasThrown = false;
+			attackWasFinished = false;
+			attackHit = false;
 		}
 		public void start(GameObject playerBodyBoxP, GameObject playerHitBoxP, GameObject playerBlockBoxP) {
 			playerBodyBox = playerBodyBoxP;
 			playerHitBox = playerHitBoxP;
 			playerBlockBox = playerBlockBoxP;
+		}
+		// --------------------------------------------------------------------------------
+		// PLAYER.THROWATTACK();
+		// Throws up the attack hitbox, but only if it needs to be thrown up.
+		// Tells the player that its attack hitbox was thrown up.
+		// --------------------------------------------------------------------------------
+		private void throwAttack(float otherPlayerXPos) {
+			if (!attackWasThrown) {
+				bool facingRight = (playerBodyBox.transform.position.x < otherPlayerXPos);
+				float height = 0.5f;
+				float playerWidth = playerBodyBox.transform.localScale.x * 0.5f;
+				float reachWidth = reach * 0.5f;
+				if (!lowAttack) {
+					height = 1.5f;
+				}
+				if (facingRight) {
+					playerHitBox.transform.position = new Vector3 (getXPos () + playerWidth + reachWidth, height, 0.0f);
+				} else {
+					playerHitBox.transform.position = new Vector3 (getXPos () - playerWidth - reachWidth, height, 0.0f);
+				}
+				attackWasThrown = true;
+			}
+		}
+		// --------------------------------------------------------------------------------
+		// PLAYER.FINISHATTACK();
+		// Throws down the attack hitbox, but only if it needs to be thrown down.
+		// Tells the player that its attack hitbox was thrown down.
+		// --------------------------------------------------------------------------------
+		private void finishAttack() {
+			if (!attackWasFinished) {
+				playerHitBox.SetActive (false);
+				playerHitBox.transform.position = new Vector3 (0.0f, -10.0f, 0.0f);
+				playerHitBox.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+				attackWasFinished = true;
+			}
+		}
+		// --------------------------------------------------------------------------------
+		// PLAYER.RECEIVEATTACK();
+		// The player receives an attack for the given amount of damage. This function
+		// should define the behavior a player goes through when they are hit by an attack.
+		// --------------------------------------------------------------------------------
+		public void receiveAttack(float damage) {
+			health = health - damage;
+		}
+		// --------------------------------------------------------------------------------
+		// PLAYER.INITIATEACTION();
+		// Initates a new attack action with the provided attributes.
+		// --------------------------------------------------------------------------------
+		private void initiateAction(float duration, float attackBegin, float attackDuration, float newReach, float newDamage, bool newLow) {
+			inputHold = true;
+			attackWasThrown = false;
+			attackWasFinished = false;
+			attackHit = false;
+			timeEnds = Time.time + duration;
+			timeAttackBegins = Time.time + attackBegin;
+			timeAttackEnds = Time.time + attackBegin + attackDuration;
+			reach = newReach;
+			attackDamage = newDamage;
+			lowAttack = newLow;
+			playerHitBox.SetActive (true);
+			playerHitBox.transform.position = new Vector3 (0.0f, -10.0f, 0.0f);
+			playerHitBox.transform.localScale = new Vector3 (reach, 1.0f, 1.0f);
 		}
 		// --------------------------------------------------------------------------------
 		// PLAYER.UPDATE();
@@ -57,40 +172,42 @@ public class GameScript : MonoBehaviour {
 		public void update(float otherPlayerXPos) {
 			if (inputHold) {
 				if (Time.time >= timeEnds) {
-					// Animation is over. Release hold and return.
-					playerHitBox.SetActive (false);
-					inputHold = false;
-					return;
-				} else if (Time.time >= timeAttackEnds) {
-					// Attack period is over. Disable attack hitbox.
-					playerHitBox.SetActive (false);
-					return;
-				} else if (Time.time >= timeAttackBegins) {
-					// Attack period has began. Enable attack hitbox.
-					bool facingRight = (playerBodyBox.transform.position.x < otherPlayerXPos);
-					float height = 0.5f;
-					float playerWidth = playerBodyBox.transform.localScale.x * 0.5f;
-					float reachWidth = reach * 0.5f;
-					if (!lowAttack) {
-						height = 1.5f;
-					}
-					if (facingRight) {
-						playerHitBox.transform.position = new Vector3 (getXPos() + playerWidth + reachWidth, height, 0.0f);
+					if (attackWasThrown) {
+						// Animation is over. Release hold and return.
+						finishAttack();
+						inputHold = false;
 					} else {
-						playerHitBox.transform.position = new Vector3 (getXPos() - playerWidth - reachWidth, height, 0.0f);
+						// Attack animation cannot end without attack being thrown for at least a frame.
+						// Throw the attack. Next frame, it will be seen that the attack was thrown.
+						throwAttack(otherPlayerXPos);
 					}
-					return;
+				} else if (Time.time >= timeAttackEnds) {
+					// Attack period over. Finish attack hitbox.
+					finishAttack();
+				} else if (Time.time >= timeAttackBegins) {
+					// Attack period began. Throw attack hitbox.
+					throwAttack(otherPlayerXPos);
 				}
 			}
 		}
 		// --------------------------------------------------------------------------------
 		// PLAYER.QUERYINPUT();
 		// queryInput() takes some limited game state data and determines based on input
-		// for the frame information on how the player is to behave. Player behavior is
+		// for the frame how the player is to behave. Player behavior is
 		// actually handled in handleInput(), which contains a collision resolution
 		// algorithm.
 		// --------------------------------------------------------------------------------
-		public void queryInput(bool player1, float otherPlayerXPos, float fv, float bvf, float rvf) {
+		/*public void queryInput(bool player1, bool isAI, float otherPlayerXPos, float fv, float bvf, float rvf) {
+			if (!inputHold) {
+				Action action = new Action ();
+				if (isAI) {
+					GameState state = new GameState (); 
+				} else {
+					return;
+				}
+			}
+		}*/
+		public void queryInput(bool player1, bool isAI, float otherPlayerXPos, float fv, float bvf, float rvf) {
 			bool lowMod = false;
 			running = false;
 			movingLeft = false;
@@ -99,42 +216,45 @@ public class GameScript : MonoBehaviour {
 			distanceMoved = 0.0f;
 			oldXPosition = playerBodyBox.transform.position.x;
 			if (!inputHold) {
-				if ((player1 && Input.GetKey (KeyCode.S)) || (!player1 && Input.GetKey (KeyCode.K))) {
-					lowMod = true;
-				}
-				if ((player1 && Input.GetKeyDown (KeyCode.Q)) || (!player1 && Input.GetKeyDown (KeyCode.U))) {
-					if (lowMod) {
-						// Do a weak lower attack
-						initiateAction(1.0f,0.25f,0.5f,1.0f,true);
-					} else {
-						// Do a weak upper attack
-						initiateAction(1.0f,0.25f,0.5f,1.0f,false);
+				if (!isAI) {
+					if ((player1 && Input.GetKey (KeyCode.S)) || (!player1 && Input.GetKey (KeyCode.K))) {
+						lowMod = true;
 					}
-					return;
-				}
-				if ((player1 && Input.GetKeyDown (KeyCode.E)) || (!player1 && Input.GetKeyDown (KeyCode.O))) {
-					if (lowMod) {
-						// Do a strong lower attack
-						initiateAction(2.0f,1.25f,0.5f,1.0f,true);
-					} else {
-						// Do a strong upper attack
-						initiateAction(2.0f,1.25f,0.5f,1.0f,false);
+					if ((player1 && Input.GetKeyDown (KeyCode.Q)) || (!player1 && Input.GetKeyDown (KeyCode.U))) {
+						if (lowMod) {
+							// Do a weak lower attack
+							initiateAction (1.0f, 0.25f, 0.5f, 1.0f, 50.0f, true);
+						} else {
+							// Do a weak upper attack
+							initiateAction (1.0f, 0.25f, 0.5f, 1.0f, 50.0f, false);
+						}
+						return;
 					}
-					return;
-				}
-				if ((player1 && Input.GetKey (KeyCode.LeftShift)) || (!player1 && Input.GetKey (KeyCode.RightShift))) {
-					running = true;
-				}
-				if ((player1 && Input.GetKey (KeyCode.A)) || (!player1 && Input.GetKey (KeyCode.J))) {
-					movingLeft = true;
-				}
-				if ((player1 && Input.GetKey (KeyCode.D)) || (!player1 && Input.GetKey (KeyCode.L))) {
-					movingRight = true;
-				}
-				if ((movingLeft == movingRight) && (movingLeft == true)) {
-					movingLeft = false;
-					movingRight = false;
-					running = false;
+					if ((player1 && Input.GetKeyDown (KeyCode.E)) || (!player1 && Input.GetKeyDown (KeyCode.O))) {
+						if (lowMod) {
+							// Do a strong lower attack
+							initiateAction (2.0f, 1.25f, 0.5f, 1.0f, 50.0f, true);
+						} else {
+							// Do a strong upper attack
+							initiateAction (2.0f, 1.25f, 0.5f, 1.0f, 50.0f, false);
+						}
+						return;
+					}
+					if ((player1 && Input.GetKey (KeyCode.LeftShift)) || (!player1 && Input.GetKey (KeyCode.RightShift))) {
+						running = true;
+					}
+					if ((player1 && Input.GetKey (KeyCode.A)) || (!player1 && Input.GetKey (KeyCode.J))) {
+						movingLeft = true;
+					}
+					if ((player1 && Input.GetKey (KeyCode.D)) || (!player1 && Input.GetKey (KeyCode.L))) {
+						movingRight = true;
+					}
+					if ((movingLeft == movingRight) && (movingLeft == true)) {
+						movingLeft = false;
+						movingRight = false;
+						running = false;
+					}
+				} else {
 				}
 			}
 			movingAway = (movingLeft && (playerBodyBox.transform.position.x < otherPlayerXPos)) || (movingRight && (playerBodyBox.transform.position.x >= otherPlayerXPos));
@@ -191,16 +311,23 @@ public class GameScript : MonoBehaviour {
 				}
 			}
 		}
-		private void initiateAction(float duration, float attackBegin, float attackDuration, float newReach, bool newLow) {
-			timeEnds = Time.time + duration;
-			timeAttackBegins = Time.time + attackBegin;
-			timeAttackEnds = Time.time + attackBegin + attackDuration;
-			reach = newReach;
-			lowAttack = newLow;
-			inputHold = true;
-			playerHitBox.SetActive (true);
-			playerHitBox.transform.position = new Vector3 (0.0f, -10.0f, 0.0f);
-			playerHitBox.transform.localScale = new Vector3 (reach, 1.0f, 1.0f);
+		public bool attackHandle() {
+			return (attackWasThrown && !attackWasFinished && !attackHit);
+		}
+		public float getHitXPos() {
+			return playerHitBox.transform.position.x;
+		}
+		public float getHalfWidth() {
+			return playerBodyBox.transform.localScale.x * 0.5f;
+		}
+		public float getHitHalfWidth() {
+			return playerHitBox.transform.localScale.x * 0.5f;
+		}
+		public void tellHit() {
+			attackHit = true;
+		}
+		public float getAttackDamage() {
+			return attackDamage;
 		}
 		public float getXPos() {
 			return playerBodyBox.transform.position.x;
@@ -220,9 +347,10 @@ public class GameScript : MonoBehaviour {
 		public bool getIsMovingAway() {
 			return movingAway;
 		}
+		public float getHeath() {
+			return health;
+		}
 	}
-	private Player player1;
-	private Player player2;
 	void Start ()
 	{
 		debugging = false;
@@ -288,11 +416,14 @@ public class GameScript : MonoBehaviour {
 			player1.update (player2.getXPos ());
 			player2.update (player1.getXPos ());
 			// QUERY PLAYER INPUT
-			player1.queryInput (true, player2.getXPos (), forwardVelocity, backwardVelocityFactor, runningVelocityFactor);
-			player2.queryInput (false, player1.getXPos (), forwardVelocity, backwardVelocityFactor, runningVelocityFactor);
+			player1.queryInput (true, player1AI, player2.getXPos (), forwardVelocity, backwardVelocityFactor, runningVelocityFactor);
+			player2.queryInput (false, player2AI, player1.getXPos (), forwardVelocity, backwardVelocityFactor, runningVelocityFactor);
 			// HANDLE PLAYER INPUT
 			player1.handleInput (player2.getOldXPos (), player2.getDistanceMoved (), player2.getIsMovingAway());
 			player2.handleInput (player1.getOldXPos (), player1.getDistanceMoved (), player1.getIsMovingAway());
+			// DO HIT DETECTION
+			handlePlayerHit (player1, player2);
+			handlePlayerHit (player2, player1);
 			if (displayDebugText) {
 				debugText.text =
 					"Player1X: " + player1.getXPos ().ToString () + " Player2X: " + player2.getXPos ().ToString () +
@@ -315,5 +446,18 @@ public class GameScript : MonoBehaviour {
 		float y = yAboveHeight + yheight;
 		float z = zAbsolute * -1.0f;
 		perspectiveCamera.transform.position = new Vector3 (x, y, z);
+	}
+	void handlePlayerHit(Player attacker, Player defender) {
+		// If the attacker is engaged in an attack that needs to be handled:
+		if (attacker.attackHandle ()) {
+			// See if the attack box is in the body box.
+			float distance = Mathf.Abs(defender.getXPos() - attacker.getHitXPos());
+			distance = distance - attacker.getHitHalfWidth() - defender.getHalfWidth();
+			if (distance <= 0.0f) {
+				// This was a hit. Handle it.
+				attacker.tellHit();
+				defender.receiveAttack (attacker.getAttackDamage ());
+			}
+		}
 	}
 }
