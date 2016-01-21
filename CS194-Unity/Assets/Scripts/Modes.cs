9ffe841e2c0e16934c1eedbc4a918188de2709fd
Christopher Billovits <cjbillov@stanford.edu>
2016-01-21 11:51:28 -0800

    Player controlling their individual movements, adding to delegate.

---
+++ CS194-Unity/Assets/Scripts/Modes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/**
+ * Utility class to describe finite state automata modes
+ * to communicate between player objects
+ */
+namespace AssemblyCSharp {
+
+	public class Modes {
+		public enum Stance {Stand, Crouch};
+		public enum Attack {HiPunch, LoPunch, HiKick, LoKick, HiBlock, LoBlock, None};
+	}
+}
+
