52bb1e2a45c563f98d22e1d3555709d194071612
GarrettGutierrez <ruzhyo2000@gmail.com>
2016-01-21 17:49:43 -0800

    Merged two branches. Moved gamescript into gamedelegate and moved player into separate classes.

--- CS194-Unity/Assets/Scripts/GameDelegate.cs
+++ CS194-Unity/Assets/Scripts/GameDelegate.cs
@@ -1,65 +1,125 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
-using System.Collections.Generic;
 
-/**
- * A GameDelegate supervises the behavior of the players,
- * creates the attack hitboxes, and generally controls the 
- * simplified game state.  
- *
- * A GameDelegate responds to messages from each player, and propagates changes
- * in health from attacks, and confirms a hit.  
- */
-namespace AssemblyCSharp {
 public class GameDelegate : MonoBehaviour {
-
-
-	private List<GameObject> players;
-	
-	/* Beginning of Lifecycle:
-	 * Instantiate the player list so that players can 
-	 * add themselves to the delegate before updating.
-	 */
-	void Awake(){ 
-		players = new List<GameObject> ();	
-		Debug.Log ("Delegate awakened");
+	private bool debugging;
+	private bool displayDebugText;
+	public GameObject player1Obj;
+	public GameObject player2Obj;
+	private PlayerController player1;
+	private PlayerController player2;
+	public Text debugText;
+	public Camera perspCamera;
+	public float perspXPadding;
+	public float perspAngle;
+	public float perspWidthMinimum;
+	void Start ()
+	{
+		debugging = false;
+		displayDebugText = false;
+		debugText.text = "";
+		perspCamera.transform.eulerAngles = new Vector3 (perspAngle, 0.0f, 0.0f);
+		player1 = player1Obj.GetComponent<PlayerController> ();
+		player2 = player2Obj.GetComponent<PlayerController> ();
 	}
-
-	void Start () {
-		// pass
-	}
-
-	void addPlayer(GameObject player){
-		players.Add (player); 
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	/**
-	 * Handles the post-frame collision actions; notifies the players
-	 *
-	 **/
-	void LateUpdate(){
-		/* TODO: replace with collision logic, make symmetric for p1 -> p2 and p2->p1 */
-
-		if (players.Count <= 1)
-			return;
-		
-		for (int i = 0; i < players.Count; i++) {
-			for (int j = 0; j < players.Count; j++) {
-				// (attacker, victim) pair
-				Pair<GameObject, GameObject> playerPair = Pair.New(players[i], players[j]);
-				// Perform hit detection...
-				bool gotHit = false;
-				if (gotHit) {
-					//logic...
-				}
+	void Update()
+	{
+		// QUIT THE GAME
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		// ENTER DEBUGGING MODE
+		if (Input.GetKeyDown (KeyCode.BackQuote)) {
+			debugging = !debugging;
+			if (!debugging) {
+				debugText.text = "";
 			}
 		}
-	
+		if (debugging) {
+			// ALTER PERSPECTIVE CAMERA ANGLE ATTRIBUTES
+			if (Input.GetKeyDown (KeyCode.I)) {
+				perspAngle = perspAngle - 0.5f;
+				perspCamera.transform.eulerAngles = new Vector3 (perspAngle, 0.0f, 0.0f);
+			}
+			if (Input.GetKeyDown (KeyCode.J)) {
+				perspXPadding = perspXPadding - 0.5f;
+			}
+			if (Input.GetKeyDown (KeyCode.K)) {
+				perspAngle = perspAngle + 0.5f;
+				perspCamera.transform.eulerAngles = new Vector3 (perspAngle, 0.0f, 0.0f);
+			}
+			if (Input.GetKeyDown (KeyCode.L)) {
+				perspXPadding = perspXPadding + 0.5f;
+			}
+			if (Input.GetKeyDown (KeyCode.Comma)) {
+				perspWidthMinimum = perspWidthMinimum - 0.5f;
+			}
+			if (Input.GetKeyDown (KeyCode.Period)) {
+				perspWidthMinimum = perspWidthMinimum + 0.5f;
+			}
+			// RESET PLAYER POSITIONS
+			if (Input.GetKeyDown (KeyCode.Semicolon)) {
+				// Later on, make it so this resets everything to default locations and statuses.
+			}
+			debugText.text =
+				"perspAngle: " + perspAngle.ToString () +
+				"\nperspXPadding: " + perspXPadding.ToString () +
+				"\nperspWidthMinimum: " + perspWidthMinimum.ToString ();
+		} else {
+			if (Input.GetKeyDown (KeyCode.Quote)) {
+				displayDebugText = !displayDebugText;
+				if (!displayDebugText) {
+					debugText.text = "";
+				}
+			}
+			// UPDATE PLAYER STATUS
+			player1.handleAutomaticUpdates (player2.getXPos ());
+			player2.handleAutomaticUpdates (player1.getXPos ());
+			// QUERY PLAYER INPUT
+			player1.queryInput (player2.getXPos ());
+			player2.queryInput (player1.getXPos ());
+			// HANDLE PLAYER INPUT
+			player1.handleInput (player2.getOldXPos (), player2.getDistanceMoved (), player2.getIsMovingAway());
+			player2.handleInput (player1.getOldXPos (), player1.getDistanceMoved (), player1.getIsMovingAway());
+			// DO HIT DETECTION
+			handlePlayerHit (player1, player2);
+			handlePlayerHit (player2, player1);
+			if (displayDebugText) {
+				debugText.text =
+					"Player1X: " + player1.getXPos ().ToString () + " Player2X: " + player2.getXPos ().ToString () +
+					"\nPlayer1Moving: " + player1.getIsMoving ().ToString () + " Player2Moving: " + player2.getIsMoving ().ToString () +
+					"\nPlayer1Away: " + player1.getIsMovingAway ().ToString () + " Player2Away: " + player2.getIsMovingAway ().ToString ();
+			}
+		}
 	}
-}
+	void LateUpdate()
+	{
+		float xTransformRad = perspCamera.transform.eulerAngles.x * Mathf.Deg2Rad;
+		float yheight = (player1.getYPos() + player2.getYPos()) * 0.5f;
+		float xwidth = Mathf.Abs(player2.getXPos() - player1.getXPos()) + perspXPadding;
+		xwidth = Mathf.Max (xwidth, perspWidthMinimum);
+		float hFOVRad = 2.0f * Mathf.Atan (Mathf.Tan (perspCamera.fieldOfView * Mathf.Deg2Rad * 0.5f) * perspCamera.aspect);
+		float distance = (0.5f * xwidth) / Mathf.Tan (hFOVRad * 0.5f);
+		float yAboveHeight = distance * Mathf.Sin (xTransformRad);
+		float zAbsolute = distance * Mathf.Cos (xTransformRad);
+		float x = (player1.getXPos() + player2.getXPos()) * 0.5f;
+		float y = yAboveHeight + yheight;
+		float z = zAbsolute * -1.0f;
+		perspCamera.transform.position = new Vector3 (x, y, z);
+	}
+	void handlePlayerHit(PlayerController attacker, PlayerController defender) {
+		// If the attacker is engaged in an attack that needs to be handled:
+		if (attacker.attackHandle ()) {
+			// See if the attack box is in the body box.
+			float distance = Mathf.Abs(defender.getXPos() - attacker.getHitXPos());
+			distance = distance - attacker.getHitHalfWidth() - defender.getHalfWidth();
+			if (distance <= 0.0f) {
+				// This was a hit. Handle it.
+				Debug.Log("Hit!");
+				attacker.tellHit();
+				defender.receiveAttack (attacker.getAttackDamage ());
+			}
+		}
+	}
 }
--- CS194-Unity/Assets/Scripts/PlayerController.cs
+++ CS194-Unity/Assets/Scripts/PlayerController.cs
@@ -1,101 +1,321 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
 
-namespace AssemblyCSharp {
 public class PlayerController : MonoBehaviour {
-
-	// variables: private by default
-	// expose "getting hit"
-	double health;
-	float moveIncrement = 0.03f; 
-	Modes.Stance stance;
-	Modes.Attack atkMode;
-	
-	// Controlling keys (for same keyboard)	
-	KeyCode Up, Down, Left, Right, Punch, Kick;
-	// Use this for initialization
-	void Start () {
-		health = 100.0;
-		stance = Modes.Stance.Stand;
-		atkMode = Modes.Attack.None;
+	// HITBOX VARIABLES
+	public GameObject playerBodyBox;
+	public GameObject playerHitBox;
+	public GameObject playerBlockBox;
+	// STAT VARIABLES
+	private float health;
+	public bool player1;
+	public bool isAI;
+	public float forwardVelocity;
+	public float backwardVelocityFactor;
+	public float runningVelocityFactor;
+	// KEYBOARD INPUT
+	KeyCode /*Up, */Down, Left, Right, Run, Attack1, Attack2/*, Attack3, Attack4*/;
+	// MOVEMENT VARIABLES
+	private bool running;
+	private bool movingLeft;
+	private bool movingRight;
+	private bool movingAway;
+	private float distanceMoved;
+	private float oldXPosition;
+	// ANIMATION VARIABLES
+	private float timeEnds;
+	private float timeAttackBegins;
+	private float timeAttackEnds;
+	private float reach;
+	private float attackDamage;
+	private bool lowAttack;
+	// INFO VARIABLES
+	private bool inputHold;
+	private bool attackWasThrown;
+	private bool attackWasFinished;
+	private bool attackHit;
+	// --------------------------------------------------------------------------------
+	// PLAYER.THROWATTACK();
+	// Throws up the attack hitbox, but only if it needs to be thrown up.
+	// Tells the player that its attack hitbox was thrown up.
+	// --------------------------------------------------------------------------------
+	private void throwAttack(float otherPlayerXPos) {
+		if (!attackWasThrown) {
+			bool facingRight = (playerBodyBox.transform.position.x < otherPlayerXPos);
+			float height = 0.5f;
+			float playerWidth = playerBodyBox.transform.localScale.x * 0.5f;
+			float reachWidth = reach * 0.5f;
+			if (!lowAttack) {
+				height = 1.5f;
+			}
+			if (facingRight) {
+				playerHitBox.transform.position = new Vector3 (getXPos () + playerWidth + reachWidth, height, 0.0f);
+			} else {
+				playerHitBox.transform.position = new Vector3 (getXPos () - playerWidth - reachWidth, height, 0.0f);
+			}
+			attackWasThrown = true;
+		}
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.FINISHATTACK();
+	// Throws down the attack hitbox, but only if it needs to be thrown down.
+	// Tells the player that its attack hitbox was thrown down.
+	// --------------------------------------------------------------------------------
+	private void finishAttack() {
+		if (!attackWasFinished) {
+			playerHitBox.SetActive (false);
+			playerHitBox.transform.position = new Vector3 (0.0f, -10.0f, 0.0f);
+			playerHitBox.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			attackWasFinished = true;
+		}
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.RECEIVEATTACK();
+	// The player receives an attack for the given amount of damage. This function
+	// should define the behavior a player goes through when they are hit by an attack.
+	// --------------------------------------------------------------------------------
+	public void receiveAttack(float damage) {
+		health = health - damage;
+		inputHold = false;
+		finishAttack ();
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.INITIATEACTION();
+	// Initates a new attack action with the provided attributes.
+	// --------------------------------------------------------------------------------
+	private void initiateAction(float duration, float attackBegin, float attackDuration, float newReach, float newDamage, bool newLow) {
+		inputHold = true;
+		attackWasThrown = false;
+		attackWasFinished = false;
+		attackHit = false;
+		timeEnds = Time.time + duration;
+		timeAttackBegins = Time.time + attackBegin;
+		timeAttackEnds = Time.time + attackBegin + attackDuration;
+		reach = newReach;
+		attackDamage = newDamage;
+		lowAttack = newLow;
+		playerHitBox.SetActive (true);
+		playerHitBox.transform.position = new Vector3 (0.0f, -10.0f, 0.0f);
+		playerHitBox.transform.localScale = new Vector3 (reach, 1.0f, 1.0f);
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.UPDATE();
+	// In this function, player status that is not dependant upon input, such as in-
+	// progress animations, are handled.
+	// --------------------------------------------------------------------------------
+	public void handleAutomaticUpdates(float otherPlayerXPos) {
+		if (inputHold) {
+			if (Time.time >= timeEnds) {
+				if (attackWasThrown) {
+					// Animation is over. Release hold and return.
+					finishAttack();
+					inputHold = false;
+				} else {
+					// Attack animation cannot end without attack being thrown for at least a frame.
+					// Throw the attack. Next frame, it will be seen that the attack was thrown.
+					throwAttack(otherPlayerXPos);
+				}
+			} else if (Time.time >= timeAttackEnds) {
+				// Attack period over. Finish attack hitbox.
+				finishAttack();
+			} else if (Time.time >= timeAttackBegins) {
+				// Attack period began. Throw attack hitbox.
+				throwAttack(otherPlayerXPos);
+			}
+		}
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.QUERYINPUT();
+	// queryInput() takes some limited game state data and determines based on input
+	// for the frame how the player is to behave. Player behavior is
+	// actually handled in handleInput(), which contains a collision resolution
+	// algorithm.
+	// --------------------------------------------------------------------------------
+	/*public void queryInput(bool player1, bool isAI, float otherPlayerXPos, float fv, float bvf, float rvf) {
+		if (!inputHold) {
+			Action action = new Action ();
+			if (isAI) {
+				GameState state = new GameState (); 
+			} else {
+				return;
+			}
+		}
+	}*/
+	public void queryInput(float otherPlayerXPos) {
+		bool lowMod = false;
+		running = false;
+		movingLeft = false;
+		movingRight = false;
+		movingAway = false;
+		distanceMoved = 0.0f;
+		oldXPosition = playerBodyBox.transform.position.x;
+		if (!inputHold) {
+			if (isAI) {
+				// QUERY AI INPUT
+				// TODO: Query the AI for input.
+			} else {
+				// QUERY KEYBOARD INPUT
+				lowMod = Input.GetKey(Down);
+				if (Input.GetKeyDown (Attack1)) {
+					if (lowMod) {
+						// Do a weak lower attack
+						initiateAction (1.0f, 0.25f, 0.5f, 1.0f, 50.0f, true);
+					} else {
+						// Do a weak upper attack
+						initiateAction (1.0f, 0.25f, 0.5f, 1.0f, 50.0f, false);
+					}
+					return;
+				}
+				if (Input.GetKeyDown (Attack2)) {
+					if (lowMod) {
+						// Do a strong lower attack
+						initiateAction (2.0f, 1.25f, 0.5f, 1.0f, 50.0f, true);
+					} else {
+						// Do a strong upper attack
+						initiateAction (2.0f, 1.25f, 0.5f, 1.0f, 50.0f, false);
+					}
+					return;
+				}
+				running = Input.GetKey (Run);
+				movingLeft = Input.GetKey (Left);
+				movingRight = Input.GetKey (Right);
+				if ((movingLeft == movingRight) && (movingLeft == true)) {
+					movingLeft = false;
+					movingRight = false;
+					running = false;
+				}
+			}
+		}
+		movingAway = (movingLeft && (playerBodyBox.transform.position.x < otherPlayerXPos)) || (movingRight && (playerBodyBox.transform.position.x >= otherPlayerXPos));
+		if (movingLeft) {
+			distanceMoved = -1.0f * forwardVelocity;
+		}
+		if (movingRight) {
+			distanceMoved = forwardVelocity;
+		}
+		if (movingAway) {
+			running = false;
+			distanceMoved = distanceMoved * backwardVelocityFactor;
+		}
+		if (running) {
+			distanceMoved = distanceMoved * runningVelocityFactor;
+		}
+		distanceMoved = distanceMoved * Time.deltaTime;
+	}
+	// --------------------------------------------------------------------------------
+	// PLAYER.HANDLEINPUT();
+	// Based on limited game state data, handleInput() handles the movement that was
+	// planned to be performed in the queryInput() phase. This function contains
+	// collision detection algorithms, ensuring that when players move their body boxes
+	// never intersect with one another.
+	// --------------------------------------------------------------------------------
+	public void handleInput(float otherOldXPos, float otherDistanceMoved, bool otherMovingAway) {
+		if (getIsMoving ()) {
+			float projectedXPos = oldXPosition + distanceMoved;
+			float projectedOtherXPos = otherOldXPos + otherDistanceMoved;
+			float projectedDistance = Mathf.Abs (projectedXPos - projectedOtherXPos);
+			// If there is going to be a collision and moving towards...
+			if (!movingAway && (projectedDistance < playerBodyBox.transform.localScale.x)) {
+				// If the other is moving away....
+				if (otherMovingAway) {
+					// Move right up to the other's projected position.
+					if (movingLeft) {
+						playerBodyBox.transform.position = new Vector3 (projectedOtherXPos + playerBodyBox.transform.localScale.x, playerBodyBox.transform.position.y, playerBodyBox.transform.position.z);
+					} else {
+						playerBodyBox.transform.position = new Vector3 (projectedOtherXPos - playerBodyBox.transform.localScale.x, playerBodyBox.transform.position.y, playerBodyBox.transform.position.z);
+					}
+				} else {
+					// They are both moving towards one another.
+					// Move a ratio of the distance between them based on how much each was supposed to move.
+					float distanceRatio = Mathf.Abs(distanceMoved) / (Mathf.Abs(distanceMoved) + Mathf.Abs(otherDistanceMoved));
+					float distanceAway = Mathf.Abs (oldXPosition - otherOldXPos) - playerBodyBox.transform.localScale.x;
+					float actualDistance = distanceAway * distanceRatio;
+					if (movingLeft) {
+						actualDistance = actualDistance * -1.0f;
+					}
+					playerBodyBox.transform.position = new Vector3 (playerBodyBox.transform.position.x + actualDistance, playerBodyBox.transform.position.y, playerBodyBox.transform.position.z);
+				}
+			} else {
+				playerBodyBox.transform.position = new Vector3 (playerBodyBox.transform.position.x + distanceMoved, playerBodyBox.transform.position.y, playerBodyBox.transform.position.z);
+			}
+		}
+	}
+	public bool attackHandle() {
+		return (attackWasThrown && !attackWasFinished && !attackHit);
+	}
+	public float getHitXPos() {
+		return playerHitBox.transform.position.x;
+	}
+	public float getHalfWidth() {
+		return playerBodyBox.transform.localScale.x * 0.5f;
+	}
+	public float getHitHalfWidth() {
+		return playerHitBox.transform.localScale.x * 0.5f;
+	}
+	public void tellHit() {
+		attackHit = true;
+	}
+	public float getAttackDamage() {
+		return attackDamage;
+	}
+	public float getXPos() {
+		return playerBodyBox.transform.position.x;
+	}
+	public float getOldXPos() {
+		return oldXPosition;
+	}
+	public float getYPos() {
+		return playerBodyBox.transform.position.y;
+	}
+	public float getDistanceMoved() {
+		return distanceMoved;
+	}
+	public bool getIsMoving() {
+		return (movingLeft || movingRight);
+	}
+	public bool getIsMovingAway() {
+		return movingAway;
+	}
+	public float getHeath() {
+		return health;
+	}
+	void Awake () {
 		
-		// Set controls
-		if (this.gameObject.name == "Player1") {
-			Up = KeyCode.W; 
+	}
+	void Start () {
+		health = 1000.0f;
+		running = false;
+		movingLeft = false;
+		movingRight = false;
+		movingAway = false;
+		distanceMoved = 0.0f;
+		oldXPosition = 0.0f;
+		timeEnds = 0.0f;
+		timeAttackBegins = 0.0f;
+		timeAttackEnds = 0.0f;
+		reach = 0.0f;
+		attackDamage = 0.0f;
+		lowAttack = false;
+		inputHold = false;
+		attackWasThrown = false;
+		attackWasFinished = false;
+		attackHit = false;
+		if (player1) {
+			//Up = KeyCode.W; 
 			Down = KeyCode.S;
 			Left = KeyCode.A;
 			Right = KeyCode.D;
-			Punch = KeyCode.R;
-			Kick = KeyCode.F;
-		} else if (this.gameObject.name == "Player2") {
-			Up = KeyCode.UpArrow;
-			Down = KeyCode.DownArrow;
-			Left = KeyCode.LeftArrow;
-			Right = KeyCode.RightArrow;
-			Punch = KeyCode.Slash;
-			Kick = KeyCode.RightAlt;
+			Run = KeyCode.LeftShift;
+			Attack1 = KeyCode.Q;
+			Attack2 = KeyCode.E;
+		} else {
+			//Up = KeyCode.I;
+			Down = KeyCode.K;
+			Left = KeyCode.J;
+			Right = KeyCode.L;
+			Run = KeyCode.RightShift;
+			Attack1 = KeyCode.U;
+			Attack2 = KeyCode.O;
 		}
-		/* GameDelegate initialized its list of players already; add self 
-		 * Unity Scripts that inherit from MonoBehaviour are run on the UI loop
-		 * and cannot be invoked off of the main thread, so this operation is synchronous
-		 * with the Delegate's existence. 
-		 */
-		GameObject.Find("GameDelegate").SendMessage("addPlayer", this.gameObject);
-		Debug.Log ("Player added"); 
 	}
-
-	/* Accessors */
-	int flipStance (){
-		stance = stance ^ Modes.Stance.Crouch;
-		return (int)stance;
-	}
-	void setAttackMode(Modes.Attack mode){
-		atkMode = mode;
-	}
-
-	Modes.Attack getAttackMode(Modes.Attack mode){
-		return atkMode;
-	}
-
-	void subtractHealth(double damage){
-		health -= damage;
-	}
-	double getHealth(){
-		return health;
-	}
-
-	/** 
-	 * Callback indicating that the enemy (passed in as an argument)
-	 * was killed. 
-	 * Use this for maintaining internal state; all game statistics are 
-	 * already handled by the GameDelegate by the time this method is called.
-	 */
-
-	void didHitEnemy(GameObject enemy){
-		// Stub: implement for custom behavior.
-	}
-	/** 
-	 * Callback indicating that the player was hit by an enemy, with a given attack.
-	 * 
-	 * Use this for maintaining internal state; all game statistics are 
-	 * already handled by the GameDelegate by the time this method is called.
-	 */
-
-	void didReceiveDamage(GameObject enemy, Modes.Attack attack){
-		// Stub: implement for custom behavior
-	}
-			
-	/* Set player controls. */
-	void Update () {
-		Transform trans = this.gameObject.transform;
-		
-		int xmove = System.Convert.ToInt32(Input.GetKey(Right)) - System.Convert.ToInt32(Input.GetKey(Left));
-		int ymove = System.Convert.ToInt32(Input.GetKey(Up)) - System.Convert.ToInt32(Input.GetKey(Down));
-
-		trans.position = new Vector3 (trans.position.x + xmove * moveIncrement, 
-									  trans.position.y + ymove * moveIncrement, 
-									  trans.position.z);
-	}
-}
 }
