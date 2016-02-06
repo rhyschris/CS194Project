8b4e4a97434db25363066ec1e23addcace2d88f4
Alex Sosa <aasosa@stanford.edu>
2016-02-05 16:22:03 -0800

    Playable AI! Runs at and attacks you: invoke with 'python dumb_aggressive.py'

--- ai/agents/dumb_aggressive.py
+++ ai/agents/dumb_aggressive.py
@@ -8,7 +8,7 @@ class DumbAggressiveAgent(Agent):
     
     def __init__(self, name="Stepp"):
         super(DumbAggressiveAgent, self).__init__(name)
-        self.hitboxsize = 1.0;
+        self.hitboxsize = 1.0 + 1.0; #width of hitbox + 2*halfwidth player  
 
     def chooseAction(self):
         return self.beAggressive()
@@ -17,11 +17,11 @@ class DumbAggressiveAgent(Agent):
     def beAggressive(self):
         if (abs(self.gameState.p1Xpos-self.gameState.p2Xpos)<self.hitboxsize):
             if (not self.gameState.p1Attacking):
-                return random.choice([Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4])
+                return random.choice([Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4,Actions.moveAway])
             elif (not self.gameState.p1High):
-                return random.choice([Actions.blockup, Actions.blockup, Actions.moveAway,Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4])
+                return random.choice([Actions.blockUp, Actions.blockUp, Actions.moveAway,Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4])
             else:
-                return random.choice([Actions.blockdown, Actions.blockup, Actions.moveAway,Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4])
+                return random.choice([Actions.blockDown, Actions.blockDown, Actions.moveAway,Actions.attack1, Actions.attack2, Actions.attack3, Actions.attack4])
 
         else:
             return random.choice([Actions.walkTowards, Actions.walkTowards, Actions.runTowards, Actions.jump])
