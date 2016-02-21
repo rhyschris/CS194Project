e739644796ffd70a9673332ac887e4e05e498964
Leah Kim <leahkim@stanford.edu>
2016-02-21 11:45:15 -0800

    Added street lamps and wrecked car to background

---
+++ CS194-Unity/Assets/Scripts/LampPost.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampPost : MonoBehaviour {
+
+	public Light street_light;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		StartCoroutine (Flicker());
+	}
+
+	IEnumerator Flicker() {
+		street_light.enabled = true;
+		yield return new WaitForSeconds (Random.Range(1.0f, 60.0f));
+		street_light.enabled = false;
+		yield return new WaitForSeconds (Random.Range(1.0f,60.0f));
+	}
+}
