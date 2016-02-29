828da63fe67be3303a44c2089507324725cce6aa
Alex Sosa <aasosa@stanford.edu>
2016-02-29 12:27:33 -0800

    added IPC for genetic Qlearning that grows off of winner's Qtable

--- ai/agents/basic_qlearn.py
+++ ai/agents/basic_qlearn.py
@@ -8,7 +8,11 @@ import random
 import sys
 import signal
 import cPickle as pickle
-from plotter import Plotter
+#from plotter import Plotter
+import socket
+
+HOST = '127.0.0.1'
+
 
 class BasicQlearnAgent(Agent):
 
@@ -22,7 +26,7 @@ class BasicQlearnAgent(Agent):
 		self.prevAction = Actions.doNothing
 		self.bodywidth = 1.0;
 
-		self.possibleXdists = [-6, -4, -3, -2, -1, 0, 1, 2, 3, 4, 6]
+		self.possibleXdists = [0, 1, 2, 3, 4,5, 6]
 
 		if (overwriteFile):
 			signal.signal(signal.SIGINT, self.signalHandler)
@@ -32,10 +36,22 @@ class BasicQlearnAgent(Agent):
 		self.makeActionDic()
 		self.Qtable = dict()
 
+
+		self.fn = 'savedQTablep1.txt'
+		self.bindport = 5555
+		if (not self.p1):
+			self.fn = 'savedQTablep2.txt'
+			self.bindport=5565
+
+		self.ipc_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)    
+		self.ipc_server.bind((HOST, self.bindport))
+    
+		self.ipc_client =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+		
 		if (not loadOldTable):
 			self.initializeTable()
 		else:
-			self.retrieveQtableFromFile()
+			self.retrieveQtableFromFile(self.fn)
 
 		# times per second to plot
 		# 0 is 'off', but we add modulo plot_freq + 1, and update
@@ -43,14 +59,14 @@ class BasicQlearnAgent(Agent):
 		self.plot_freq = plot_freq + 1
 		self.plot_counter = 0
 
-		if plot_freq > 0:
-			self.plotter = Plotter()
+#		if plot_freq > 0:
+#			self.plotter = Plotter()
 
 		print("Done initializing!")
 
 	def signalHandler(self,err,ernum):
 		print("dumping to file!")
-		self.dumpQtableToFile()
+		self.dumpQtableToFile(self.fn)
 		sys.exit(0)
 
 	def makeActionDic(self):
@@ -75,12 +91,10 @@ class BasicQlearnAgent(Agent):
 
 
 	def getGSTuple(self,gamestate):
-		xdist = int(gamestate.p1Xpos-gamestate.p2Xpos-self.bodywidth)
+		xdist = abs(int(gamestate.p1Xpos-gamestate.p2Xpos-self.bodywidth))
 
 		tupXdist =xdist
-		if (xdist<-4):
-			tupXdist = -6
-		elif(xdist>4):
+		if(xdist>5):
 			tupXdist = 6
 
 		ydist = int(gamestate.p1Ypos-gamestate.p2Ypos)
@@ -92,7 +106,7 @@ class BasicQlearnAgent(Agent):
 
 		return (tupXdist,tupYdist,gamestate.actionFlags)
 
-	def getQRow(self,gamestate):
+	def getQRow(self,gamestate):	
 		tup = self.getGSTuple(gamestate)
 
 		return self.Qtable[tup]
@@ -119,6 +133,7 @@ class BasicQlearnAgent(Agent):
 
 		if (p1win):
 			p2damage = 250
+			
 		if (p2win):
 			p1damage = 250
 
@@ -140,6 +155,30 @@ class BasicQlearnAgent(Agent):
 
 
 		self.prevGamestate = gameState
+		if (p1win):
+			self.prevGamestate = GameState(-4,0,100,4,0,100)
+			if (self.p1):
+				dumpQtableToFile('geneticQtableFile.txt') #dump to mutual file
+				data = bytearray()
+				data.append(255)
+				print("sending ipc")
+				self.ipc_client.sendto(data, (HOST, self.bindport+10))	#let other player know file is ready
+			else:
+				data, addr = self.ipc_server.recvfrom(1) 
+				print('retrieved ipc')
+				retrieveQtableFromFile('geneticQtableFile.txt')
+		if (p2win):
+			self.prevGamestate = GameState(-4,0,100,4,0,100)
+			if (not self.p1):
+				dumpQtableToFile('geneticQtableFile.txt') #dump to mutual file
+				data = bytearray()
+				data.append(255)
+				self.ipc_client.sendto(data, (HOST, self.bindport-10))	#let other player know file is ready
+			else:
+				data, addr = self.ipc_server.recvfrom(1) 
+				print('got ipc')
+				retrieveQtableFromFile('geneticQtableFile.txt')
+
 
 	def chooseAction(self):
 		qRow = self.getQRow(self.prevGamestate)
@@ -147,8 +186,8 @@ class BasicQlearnAgent(Agent):
 		# update plot according to frequency
 		self.plot_counter = (self.plot_counter + 1) % (self.plot_freq + 1)
 
-		if self.plot_counter == 1:
-			self.plotter.updateGraph(qRow)
+		#if self.plot_counter == 1:
+			#self.plotter.updateGraph(qRow)
 
 		count = qRow.count(maxQ)
 		action = None
@@ -158,7 +197,7 @@ class BasicQlearnAgent(Agent):
 			action = random.choice(self.actions) 
 		elif count > 1:
 			best = [i for i in range(len(self.actions)) if qRow[i] == maxQ]
-			print("ranomly chose an action out of best possible")
+			print("randomly chose an action out of best possible")
 			action = self.actions[random.choice(best)]
 		else:
 			print("maxq = ",maxQ)
@@ -168,19 +207,21 @@ class BasicQlearnAgent(Agent):
 		self.epsilon-=0.00001
 		return action
 
-	def dumpQtableToFile(self):
-		filename = "savedQTablep2.txt"
-		if self.p1:
-			filename="savedQTablep1.txt"
+	def dumpQtableToFile(self,filename):
+		#filename = "savedQTablep2.txt"
+		#if self.p1:
+		#	filename="savedQTablep1.txt"
 		with open(filename, "wb") as myFile:
 			pickle.dump(self.Qtable, myFile)
 
-	def retrieveQtableFromFile(self):
-		filename = "savedQTablep2.txt"
-		if self.p1:
-			filename="savedQTablep1.txt"
+	def retrieveQtableFromFile(self,filename):
+		#filename = "savedQTablep2.txt"
+		#if self.p1:
+		#	filename="savedQTablep1.txt"
 		with open(filename, "rb") as myFile:
-			self.Qtable = pickle.load(myyFile)
+			self.Qtable = pickle.load(myFile)
+
+
 
 
 if __name__ == '__main__':
@@ -191,7 +232,7 @@ if __name__ == '__main__':
     
         p1 = False
 
-    agent = BasicQlearnAgent(p1, loadOldTable=True,epsilon=.75, 
+    agent = BasicQlearnAgent(p1, loadOldTable=False,epsilon=.15, 
     						overwriteFile=True, plot_freq=5)
 
     print "Agent {0} reporting for duty".format(agent.name)
