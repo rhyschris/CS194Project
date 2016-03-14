69806ce2592a699defe6251b7f31e2c62bc4fed3
Andrew Lee <andrewlee@DN0a22b202.SUNet>
2016-03-14 15:38:59 -0700

    Changes to player controller that was lost in previous merge

--- CS194-Unity/Assets/Scripts/PlayerController.cs
+++ CS194-Unity/Assets/Scripts/PlayerController.cs
@@ -247,10 +247,20 @@ public class PlayerController : MonoBehaviour {
 		} else {
 			fighterAnimator.SetBool ("runForward", false);
 		}
+		if (myAction.actionType == ActionType.runTowards) {
+			fighterAnimator.SetBool ("sprinting", true);
+		} else {
+			fighterAnimator.SetBool ("sprinting", false);
+		}
 		if (myAction.actionType == ActionType.moveAway) {
 			fighterAnimator.SetBool ("runBackward", true);
 		} else {
 			fighterAnimator.SetBool ("runBackward", false);
+		}
+		if (myAction.actionType == ActionType.blockUp || myAction.actionType == ActionType.blockDown) {
+			fighterAnimator.SetBool ("block", true);
+		} else {
+			fighterAnimator.SetBool ("block", false);
 		}
 
 
